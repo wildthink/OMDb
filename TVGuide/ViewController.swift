3cbc58567aaa8473476ae98804206a247124118f
Jason Jobe <github@jasonjobe.com>
2015-12-10 17:51:47 -0500

    Initial Commit

---
+++ TVGuide/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TVGuide
+//
+//  Created by Jason Jobe on 12/10/15.
+//  Copyright © 2015 WildThink. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
